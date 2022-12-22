4ebde3eed36b65307b4b9328ba509c380a36a725
wtripp <will.tripp@gmail.com>
2022-12-21 21:17:58 -0500

    Populate User and Show models

--- tv/admin.py
+++ tv/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Show
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username","first_name", "last_name")
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ("id","name","date_added")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Show, ShowAdmin)
--- tv/models.py
+++ tv/models.py
@@ -1,16 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class User(AbstractUser):
-
-    def __str__(self):
-        return f"{self.username}"
-
-
 class Show(models.Model):
 
+    id = models.IntegerField(primary_key=True)
     name = models.TextField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name}"
+
+
+class User(AbstractUser):
+
+    shows = models.ManyToManyField(Show, related_name="trackers")
+
+    def __str__(self):
+        return f"{self.username}"
