e9aeb2f9f850bb506bd99dc9a90d2f12bdb9e613
wtripp <will.tripp@gmail.com>
2022-12-26 15:10:27 -0500

    Implement sort functionality

--- tv/static/tv/script.js
+++ tv/static/tv/script.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  const nameSortButton = document.getElementById("name-sort");
+  const channelSortButton = document.getElementById("channel-sort");
+  const airdateSortButton = document.getElementById("airdate-sort");
+
+  //sort_by_name(nameSortButton);
+  sort_shows(airdateSortButton, ".show-airdate");
+
   document.querySelectorAll('.track-button').forEach(button => {
     button.onclick = function() {
         const show = this.closest('[id^="show-"]');
         track(show);
+
     }
   });
+
+  nameSortButton.addEventListener('click', () => sort_shows(nameSortButton, ".show-name"));
+  channelSortButton.addEventListener('click', () => sort_shows(channelSortButton, ".show-channel"));
+  airdateSortButton.addEventListener('click', () => sort_shows(airdateSortButton, ".show-airdate"));
+
 });
 
 
@@ -36,9 +49,72 @@ function track(show) {
     });
 
     button.innerHTML = "Track";
+  }
+}
 
+/* Sort shows based on a sorting class key and update the button toggle. */
+function sort_shows(button, sortKeyClass) {
 
+  // Get show summaries.
+  const summaries = document.querySelectorAll('.show-summary');
 
+  // Get text of the keys to sort by.
+  const keys = [];
+  for (const summary of summaries) {
+    const key = summary.querySelector(sortKeyClass).textContent;
+    keys.push(key);
+  }
 
+  // Sort the keys.
+  if (sortKeyClass === ".show-airdate") {
+
+    var sortedKeys = keys.sort((a, b) => {
+      const dateA = new Date(a);
+      const dateB = new Date(b);
+      return dateB - dateA;  // most recent dates first
+    });
+
+  } else {
+    var sortedKeys = keys.sort();
+  }
+
+  // Get the summaries sorted by the show titles.
+  const sortedSummaries = [];
+  for (const sortedKey of sortedKeys) {
+    for (const summary of summaries) {
+      const key = summary.querySelector(sortKeyClass).textContent;
+      if (key === sortedKey) {
+        sortedSummaries.push(summary);
+      }
+    }
+  }
+
+  // Sort ascending or descending.
+  const parent = summaries[0].parentNode;
+  const upArrow = button.querySelector(".up-arrow");
+  const downArrow = button.querySelector(".down-arrow");
+  let sortAscending = downArrow.classList.contains("active-sort")
+  let sortDescending = upArrow.classList.contains("active-sort")
+  if (!sortAscending && !sortDescending) {
+    sortAscending = true;
+  }
+
+  if (sortAscending) {
+    for (const sortedSummary of sortedSummaries) {
+      parent.insertBefore(sortedSummary, parent.lastChild);
+      upArrow.classList.add("active-sort");
+      downArrow.classList.remove("active-sort");
+    }
+    upArrow.style.fontWeight = "bold";
+    downArrow.style.fontWeight = "";
+
+  } else if (sortDescending) {
+    for (const sortedSummary of sortedSummaries) {
+      parent.insertBefore(sortedSummary, parent.firstChild);
+      upArrow.classList.remove("active-sort");
+      downArrow.classList.add("active-sort");
+    }
+    upArrow.style.fontWeight = "";
+    downArrow.style.fontWeight = "bold";
   }
 }
