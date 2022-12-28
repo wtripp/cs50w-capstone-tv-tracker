87c629127f3a7ac54f49c93cda8632bc73a34c85
wtripp <will.tripp@gmail.com>
2022-12-28 04:48:37 -0500

    Revert "Expand/collapse hamburger menu"
    This reverts commit ccbbc79398d640e711734af9f6e734e46ab56022.

--- tv/static/tv/menu.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('navSortFilterButton');
-const menu = document.getElementById('navSortFilter');
-
-button.addEventListener('click', function () {
-  menu.classList.toggle('show');
-  button.classList.toggle('collapsed');
-});
